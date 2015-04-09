7651333f137eb441cbac0c84611bff27430a9125
Gaurav Mahajan <gauravmmh@gmail.com>
2015-04-09 14:29:05 +0530

    SLL: Find middle element
    Implemented an algorithm to find a middle element of a SLL.
    
    Signed-off-by: Gaurav Mahajan <gauravmmh@gmail.com>

---
+++ sll/middle_element_of_sll/README.md
@@ -0,0 +1,39 @@
+# Find middle element of sll 
+
+## Problem statement
+Give a sll find a node in sll which divides the sll into two equal parts.
+i.e Find the center of the linked list.
+
+OR
+
+Divide the give sll in two parts from the center of sll.
+
+## Solution
+We have give a head of sll.
+To find the center element of the sll we take 2 pointer which will traverse
+the linked list.
+
+One poiter (slow) will traverse the list one node at a time.
+```
+slow = slow->next
+```
+Another pointer (fast)will traverse the sll two nodes at a time.
+```
+fast = fast->next->next
+```
+So when the fast pointer reaches the end of the sll the slow pointer reachec
+the center of sll.
+
+### Time complexity.
+In best/worst/average case we are travesring the complete list once to find
+the middle element. So the time compexity is O(n).
+
+## To Compile
+```
+gcc -L../  middle_element_of_sll.c  -o middle_element_of_sll.o -lsll
+```
+## To Run
+```
+export LD_LIBRARY_PATH=`pwd`"/../"
+./middle_element_of_sll.o
+```
---
+++ sll/middle_element_of_sll/middle_element_of_sll.c
@@ -0,0 +1,94 @@
+/******************************************************************************
+ *
+ * Geeksforgeeks-solutions
+ *
+ * Name: sll/delete_node_using_pointer/delete_node_using_pointer.c
+ * Module: sll
+ * Created: Gaurav Mahajan <gauravmmh@gmail.com>
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "../include/sll.h"
+
+/**
+ * sll_get_middile_element: This function return the middle element of the
+ *			    sll. *
+ *
+ * @head: head of the sll whose middle element is to be found.
+ *
+ * @return: Returns the pointer for middle element of sll.
+ *	    1. length(sll) =  1 : head will be returned.
+ *	    2. lenght(sll) = even : first element of the second part is
+ *				    returned.
+ *	    3. length(sll) = odd : The returned elemet is actual middle
+ *				   element
+ */
+struct sll_node *
+sll_get_middle_element(struct sll_node *head)
+{
+	struct sll_node *slow= NULL, *fast = NULL;
+
+	if(!head)
+		return NULL;
+
+	if (head->next == NULL) {
+		return head;
+	} else {
+		slow = fast = head;
+		while((fast) && (fast->next)) {
+			fast = fast->next->next; /* 2 steps */
+			slow = slow->next; /* 1 step */
+		}
+
+	}
+	return slow;
+}
+
+/*		-: Middle of SLL :-
+ * Example
+ *
+ * odd length sll
+ * 1->2->3->null
+ *    s  f
+ *
+ * even lenght sll
+ * 1->2->3->4->null
+ *    s  f
+ *	 s       f
+ *
+ */
+
+void
+main()
+{
+	struct sll_node *h = NULL;
+	struct sll_node *mid = NULL;
+
+	/* insert at head */
+	h = sll_add_at_head(h, 1);
+	h = sll_add_at_head(h, 2);
+        h = sll_add_at_head(h, 3);
+        h = sll_add_at_head(h, 4);
+        h = sll_add_at_head(h, 5);
+
+	printf("SLL : ");
+	sll_print(h);
+
+	mid = sll_get_middle_element(h);
+
+	printf("Odd SLL: middle_element: %d\n",
+		mid->data);
+
+	/* Delete head */
+	printf("Delete Head: ");
+	h = sll_delete_at_index(h, 0);
+	sll_print(h);
+
+	printf("Even SLL: middle_element: %d\n",
+		(sll_get_middle_element(h)->data));
+
+	sll_destroy(h);
+}
